763eb0c5e00bfb70154fbfd8cef430498a1ccf05
koheesohee-hub <koheesohee@gmail.com>
2025-12-11 21:48:58 +0600

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,4 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
 
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+int main(void){
+    struct Node *head = NULL;
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+
+    struct Node *temp = head;
+    while(temp != NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
+
+//another basic c code  example 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -116,3 +158,7 @@ int main() {
 
     return 0;
 }
+
+
+
+
